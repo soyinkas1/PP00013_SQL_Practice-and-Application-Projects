9a26d869777a48d61f151bbc447620e81d57a5fb
unknown <Soyinka Sowoolu>
2024-02-14 23:56:28 +0000

    SQL for DA Book_Time Series Query Script updated

--- SQL for DA Book Walkalong Queries/SQL for DA Book_Time Series Chapter.sql
+++ SQL for DA Book Walkalong Queries/SQL for DA Book_Time Series Chapter.sql
@@ -105,3 +105,37 @@ WHERE kind_of_business in ('Men''s clothing stores', 'Women''s clothing stores')
 GROUP BY 1
 ORDER BY 1
 ;
+
+-- Find difference, ratios, and percent diffence between time series in the data set
+SELECT
+	sales_year,
+	womens_sales - mens_sales as womens_minus_mens,
+	mens_sales - womens_sales as mens_minus_womens
+FROM
+	(
+	SELECT date_part('year', sales_month) as sales_year,
+		sum(case when kind_of_business = 'Women''s clothing stores' then sales end) as womens_sales,
+		sum(case when kind_of_business = 'Men''s clothing stores' then sales end) as mens_sales
+	FROM retail_sales
+	WHERE kind_of_business in ('Men''s clothing stores', 
+							   'Women''s clothing stores')
+							   and sales_month <= '2019-12-01'
+	GROUP BY 1
+	) as a
+ORDER BY 1
+;
+
+-- Calculating the difference without the subquery
+SELECT
+	date_part('year', sales_month) as sales_year,
+	sum(case when kind_of_business = 'Women''s clothing stores' then sales end)
+		-
+	sum(case when kind_of_business = 'Men''s clothing stores' then sales end) 
+	as womens_minus_mens
+FROM retail_sales
+WHERE kind_of_business in ('Women''s clothing stores', 
+						  'Men''s clothing stores')
+						  and sales_month <= '2019-12-01'
+GROUP BY 1
+ORDER BY 1
+;
